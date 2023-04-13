276b7f21d2ee7b9e4caa4f2cfb57b7347398e754
Guillermo Santos <guillermo.santosba@gmail.com>
2023-04-12 20:05:15 -0600

    Empezando a implementar el comando put

--- HFile.py
+++ HFile.py
@@ -1,7 +1,10 @@
 
+import time
+
 class HFile:
 		''' Class for handling files '''
 		''' An HFile has an array of columns and an array of rows '''
+
 		
 		def __init__(self, name, columns, rows):
 			self.name = name
@@ -10,17 +13,35 @@ class HFile:
 
 		
 		# Getters
-	
 		def get_name(self):
 			return self.name
-		
 		def get_columns(self):
-			return self.columns
-		
+			return self.columns	
 		def get_rows(self):
-			return self.rows
+			return self.rows	
+		
+
+		# HBase methods
+		def create_row(self, key, column, value):
+			''' Creates a row with the given key, column and value the tiemestamp is set to current time '''
+			''' Returns the row created '''
+			row = Row(key, column, time.time(), value, True)
+			self.rows.append(row)
+			return row
+
+
+class Row:
+	''' Class for handling rows '''
+	''' A row has a key, column name, timestamp, value and a boolean indicating if it is enabled '''
+
+	def __init__(self, key, column, timestamp, value, enabled):
+		self.key = key
+		self.column = column
+		self.timestamp = timestamp
+		self.value = value
+		self.enabled = enabled
 	
-		
-		
-		
+	# Method for disabling a row
+	def disable(self):
+		self.enabled = False
 						
